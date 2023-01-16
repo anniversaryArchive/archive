718a40ea81b1496bc38f2961956cdc5e1d508dbe
sjkim <sojungkim318@gmail.com>
2023-01-16 17:45:46 +0900

    Feat : Date Input + SelectBox 컴포넌트 추가

---
+++ src/components/common/dataPicker.vue
@@ -0,0 +1,100 @@
+<template>
+    <div>
+        <q-input ref="inputRef" v-model="val" :clearable="clearable" :dense="true"
+                 :mask='maskFormat' :readonly="readonly" >
+            <template v-slot:append>
+                <q-icon class="cursor-pointer" name="event">
+                    <q-popup-proxy cover transition-hide="scale" transition-show="scale">
+                        <q-date v-model="val" :emit-immediately="false" :locale="changeMonthToNum" :today-btn="true" :value="val"
+                                mask="YYYY-MM-DD">
+                            <div class="row items-center justify-end">
+                                <q-btn v-close-popup color="primary" flat label="Close"/>
+                            </div>
+                        </q-date>
+                    </q-popup-proxy>
+                </q-icon>
+            </template>
+        </q-input>
+    </div>
+</template>
+
+<script>
+import {computed, defineComponent, ref} from 'vue';
+import cscript from '@/composables/comScripts';
+import moment from 'moment/moment';
+
+export default defineComponent({
+    name : 'DatePicker',
+    emits: ['update:modelValue'],
+    props: {
+        modelValue      : {
+            type   : String,
+            default: () => (''),
+        },
+        id              : {
+            type    : String,
+            required: false,
+        },
+        disabled        : {
+            required: false,
+            default : false,
+        },
+        readonly        : {
+            required: false,
+            default : false,
+        },
+        clearable       : {
+            required: false,
+            default : false,
+        },
+        dateFormat      : {
+            require: false,
+            default: () => ('YYYY-MM-DD'),
+        },
+        returnDataFormat: {
+            require: false,
+            default: () => ('YYYYMMDD'),
+        },
+    },
+    setup(props, {emit}) {
+        const maskFormat = props.dateFormat.replace(/[a-z]/gi, '#');
+        // console.log('maskFormat : ', maskFormat);
+        const changeMonthToNum = {
+            days       : ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+            daysShort  : ['일', '월', '화', '수', '목', '금', '토'],
+            months     : ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+            monthsShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        };
+        const inputRef = ref(null); // 마운트 이후 바인딩
+        const val = computed({
+            get: () => getVal(props.modelValue),
+            set: (value) => emit('update:modelValue', setVal(value)),
+        });
+
+        function getVal() {
+            let resultVal = '';
+
+            if (!cscript.$isEmpty(props.modelValue)) {
+                resultVal = moment(props.modelValue).format(props.returnDataFormat).toString() === props.modelValue ? moment(props.modelValue).format(props.dateFormat).toString() : props.modelValue;
+            }
+
+            return resultVal;
+        }
+
+        function setVal(value) {
+            let resultVal = '';
+            if (!cscript.$isEmpty(value)) {
+                resultVal = moment(value).format(props.dateFormat).toString() === value ? moment(value).format(props.returnDataFormat).toString() : value;
+            }
+            return resultVal;
+        }
+
+        return {
+            val,
+            maskFormat,
+            inputRef,
+            changeMonthToNum,
+        };
+    },
+});
+</script>
--- src/pages/Group.vue
+++ src/pages/Group.vue
@@ -1,11 +1,60 @@
 <template>
     <div>
-        <HelloWorld msg="Group Page" />
+        Group Page
+        <br>
+        <date-picker :id="groupParams.debutDate" v-model='groupParams.debutDate' :clearable='true'/>
+        <br>
+        <select-box id="artist" v-model='groupParams.artist'
+                        v-bind='selectBoxOptions.artist' />
+        <br>
+        {{groupParams}}
     </div>
 </template>
 
-<script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+import ccobject from '@/composables/createComObject';
+import cinitial from '@/composables/comInitialize';
+import cscript from '@/composables/comScripts';
+// import {useQuasar} from 'quasar';
+import DatePicker from '../components/common/dataPicker.vue';
+import SelectBox from  '../components/common/selectBox.vue';
+import {GroupParams, GroupParamsType} from '@/types/Group';
+
+export default defineComponent({
+    name        : 'Group',
+    components  : {DatePicker, SelectBox},
+    setup(){
+        // const $q = useQuasar();
+        const groupParams = ref({} as GroupParams);
+        const {selectBoxOptions: selectBoxOptions} = ccobject.$createSelectAll(['artist']);
+
+        // 데이터 초기화
+        groupParams.value = cinitial.$inItData('', GroupParamsType) as unknown as GroupParams;
+
+        (async () => {
+            // 아티스트 정보 가져오기
+            const tempList = [{
+                'codeNm': '김원필',
+                'codeValue': '940428'
+            }];
+
+            // 아티스트 셀렉트 옵션
+            selectBoxOptions.value.artist = {
+                name     : 'artistStatusOptions',
+                clearable: true,
+                style    : 'width: 250px',
+            };
+            selectBoxOptions.value.artist.data = await cscript.$getComboOptions(tempList);
+        })();
+
+        return{
+            groupParams,
+            selectBoxOptions
+        };
+    }
+});
+
 </script>
 
 <style scoped>
