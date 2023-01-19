7d582f039e7ac23a7e117b6a87d577853f631e4d
sjkim <sojungkim318@gmail.com>
2023-01-19 16:59:38 +0900

    Feat : 공통 페이지 타이틀 생성

---
+++ src/components/common/ComFuncButtons.vue
@@ -0,0 +1,42 @@
+<template>
+    <q-btn :class='btnClass' :disabled='dis' type='button'
+           @click='$emit("gfnCallFunc",id,{},$event)'>
+        {{ name }}
+    </q-btn>
+</template>
+
+<script lang="ts">
+import {computed, defineComponent} from 'vue';
+
+export default defineComponent({
+    name      : 'ComFuncButtons',
+    props     : ['id', 'name', 'type', 'dis'],
+    emits: ['update:modelValue', 'gfnCallFunc'],
+    setup(props) {
+        const btnClass = computed(() => {
+            let btnClassName;
+
+            switch (props.type) {
+                //	공통 영역 버튼
+                case 'inquire'  :	//	조회
+                case 'create'   :	//	신규
+                case 'delete'   :	//	삭제
+                case 'save'     :	//	저장
+                    btnClassName = 'btn-' + props.type;
+                    break;
+                //	그 외에는 type 을 class 명 으로 받음
+                default :
+                    btnClassName = props.type;
+                    break;
+            }
+            return btnClassName;
+        });
+
+        return {
+            btnClass,
+        };
+
+    },
+
+});
+</script>
--- src/types/CommonTypes.ts
+++ src/types/CommonTypes.ts
@@ -28,3 +28,40 @@ export interface CommonCode extends CommonRowData {
     codeValue: string,
     codeNm: string,
 }
+
+export interface FuncButtons {
+    id: string;
+    name: string;
+    type: string;
+    stat: boolean;
+    dis: boolean;
+    hide: boolean;
+}
+
+export interface AuthButtons {
+    inquire: boolean;
+    create: boolean;
+    delete: boolean;
+    save: boolean;
+    excel: boolean;
+    skll1: boolean;
+    skll2: boolean;
+    skll3: boolean;
+}
+
+export interface ButtonAuths extends AuthButtons {
+    menuId: string;
+    authorGroup: string;
+}
+
+const AuthButtonsType: { [key: string]: string } = {
+    inquire: ' boolean',
+    create : ' boolean',
+    delete : ' boolean',
+    save   : ' boolean',
+    excel  : ' boolean',
+    skll1  : ' boolean',
+    skll2  : ' boolean',
+    skll3  : ' boolean',
+};
+export {AuthButtonsType};
