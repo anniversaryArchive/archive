ea7beb687619d3059fc647563f0868ca492ed498
hyejin-1010 <hyepago@gmail.com>
2023-02-11 16:57:18 +0900

    Style: comboBox component

--- src/components/common/comboBox.vue
+++ src/components/common/comboBox.vue
@@ -1,37 +1,32 @@
 <template>
   <Combobox :modelValue="value" @update:modelValue="onChange">
     <div class="relative mt-1">
-      <div class="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+      <div class="relative w-full overflow-hidden text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
         <ComboboxInput
-          class="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+          class="w-full py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 border-none focus:ring-0"
           :displayValue="(option) => option.name"
           @change="query = $event.target.value" />
         <ComboboxButton class="absolute inset-y-0 right-0 flex items-center pr-2">
-          DOWN
-          <!--
-          <ChevronUpDownIcon
-            class="h-5 w-5 text-gray-400"
-            aria-hidden="true" />
-          -->
-          </ComboboxButton>
+          <q-icon name="expand_more" />
+        </ComboboxButton>
       </div>
       <TransitionRoot
         leave="transition ease-in duration-100"
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
         @after-leave="query = ''">
-        <ComboboxOptions class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+        <ComboboxOptions class="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
           <div v-if="filterOptions.length === 0 && query !== ''"
-            class="relative cursor-default select-none py-2 px-4 text-gray-700">
+            class="relative px-4 py-2 text-gray-700 cursor-default select-none">
             Nothing found.
           </div>
 
           <ComboboxOption v-for="option in filterOptions"
             as="template" :key="option.id"
             :value="option" v-slot="{ selected, active }">
-            <li class="relative cursor-default select-none py-2 pl-10 pr-4"
+            <li class="relative py-2 pl-10 pr-4 cursor-default select-none"
               :class="{
-                'bg-teal-600 text-white': active,
+                'bg-blue-200 text-bold': active,
                 'text-gray-900': !active,
               }">
               <span class="block truncate"
@@ -40,11 +35,8 @@
               </span>
               <span v-if="selected"
                 class="absolute inset-y-0 left-0 flex items-center pl-3"
-                :class="{ 'text-white': active, 'text-teal-600': !active }">
-                <!--
-                <CheckIcon class="h-5 w-5" aria-hidden="true" />
-                -->
-                CHECK
+                :class="{ 'font-bold': active, 'text-blue-400': !active }">
+                <q-icon name="done" />
               </span>
             </li>
           </ComboboxOption>
