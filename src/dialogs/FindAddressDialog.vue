69bb345b966220b00613929e696821b312fe075f
hyejin-1010 <hyepago@gmail.com>
2023-02-25 23:04:34 +0900

    Feat: FindAddressDialog

---
+++ src/dialogs/FindAddressDialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <CommonDialog :show="show" @close="closeDialog">
+    <VueDaumPostcode @complete="onCompleteAddress" />
+  </CommonDialog>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref, computed, ComputedRef } from 'vue';
+import CommonDialog from './CommonDialog.vue';
+import { VueDaumPostcode, VueDaumPostcodeCompleteResult } from 'vue-daum-postcode';
+
+interface Props {
+  show: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), { });
+const emit = defineEmits(['done', 'close']);
+
+function onCompleteAddress (result: VueDaumPostcodeCompleteResult) {
+  emit('done', result);
+}
+
+function closeDialog () {
+  emit('close')
+}
+</script>
+
+<style scoped>
+
+</style>
