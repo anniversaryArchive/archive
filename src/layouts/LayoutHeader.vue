c6b6924a4d2f42e34281fd2ed5b198ba6903c959
sjkim <sojungkim318@gmail.com>
2023-05-06 14:39:45 +0900

    Feat : 상단 메뉴 추가

--- src/layouts/LayoutHeader.vue
+++ src/layouts/LayoutHeader.vue
@@ -3,71 +3,74 @@
     <q-header bordered class="bg-white text-primary">
       <q-toolbar>
         <q-toolbar-title class="toolbar-title">
-          <!-- <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
-          </q-avatar>
-          -->
-          <span class="layout-title">ARCHIVE</span>
+          <span class="layout-title" @click="$router.push('/')" style="cursor: pointer">ARCHIVE</span>
         </q-toolbar-title>
 
-        <q-btn v-if="loggedIn" @click="doLogout">로그아웃</q-btn>
-        <q-btn v-else @click="doLogin">로그인</q-btn>
+        <q-space />
+
+        <div v-if="$q.screen.sizes.md <= $q.screen.width">
+          <ul class="menu-ul">
+            <li>
+              <q-btn v-if="loggedIn" @click="doLogout">로그아웃</q-btn>
+              <q-btn v-else @click="doLogin">로그인</q-btn>
+            </li>
+            <li>
+              <q-btn @click="$router.push('/favorite')" flat>즐겨찾기</q-btn>
+            </li>
+            <li>
+              <q-btn flat>소통창구</q-btn>
+            </li>
+          </ul>
+        </div>
       </q-toolbar>
     </q-header>
     <div id="signinBox" style="display: none"></div>
   </q-layout>
 
-  <SignInDialog :show="isOpenSignInDialog"
-    @close="() => { isOpenSignInDialog = false; }" />
+  <SignInDialog
+      :show="isOpenSignInDialog"
+      @close="
+      () => {
+        isOpenSignInDialog = false
+      }
+    "
+  />
 </template>
 
-<script lang='ts'>
-import { defineComponent, ref, computed } from 'vue';
-import { useUserStore } from '@/stores/user';
-import SignInDialog from '@/dialogs/SignInDialog.vue';
+<script lang="ts">
+import {defineComponent, ref, computed} from "vue"
+import {useQuasar} from "quasar"
+import {useUserStore} from "@/stores/user"
+import SignInDialog from "@/dialogs/SignInDialog.vue"
 
 export default defineComponent({
-  name: 'LayoutHeader.',
-  components: { SignInDialog },
+  name: "LayoutHeader.",
+  components: {SignInDialog},
   setup() {
-    const userStore = useUserStore();
-    const loggedIn = computed(() => userStore.loggedIn);
+    const $q = useQuasar()
+    $q.screen.setSizes({sm: 360, md: 800, lg: 1500, xl: 2000})
 
-    const isOpenSignInDialog = ref(false);
+    const userStore = useUserStore()
+    const loggedIn = computed(() => userStore.loggedIn)
+    const isOpenSignInDialog = ref(false)
+
     return {
       userStore,
-      loggedIn,           // 현재 로그인 여부 
-      isOpenSignInDialog,  // 로그인/회원가입 Dialog open 여부 
-    };
+      loggedIn, // 현재 로그인 여부
+      isOpenSignInDialog // 로그인/회원가입 Dialog open 여부
+    }
   },
   methods: {
-    // 로그인 버튼 클릭 시, 로그인/회원가입 Dialog을 띄워준다 
+    // 로그인 버튼 클릭 시, 로그인/회원가입 Dialog을 띄워준다
     doLogin() {
-      this.isOpenSignInDialog = true;
+      this.isOpenSignInDialog = true
     },
-    // 로그아웃 버튼 클릭 시, userStore.doLogout을 호출하여 로그아웃을 진행한다. 
+    // 로그아웃 버튼 클릭 시, userStore.doLogout을 호출하여 로그아웃을 진행한다.
     doLogout() {
-      this.userStore.doLogout();
-    },
-  },
-});
-</script>
-
-<style scoped>
-  .toolbar-title {
-    text-align: left;
-  }
-
-  .layout-title {
-    font-style: normal;
-    font-weight: 900;
-    font-size: 2rem;
-    line-height: 60px;
-  }
-
-  @media screen and (max-width: 767px){
-    .toolbar-title {
-      text-align: center;
+      this.userStore.doLogout()
     }
   }
-</style>
+})
+</script>
+
+<style scoped></style>
