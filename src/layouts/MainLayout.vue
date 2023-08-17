90473f5e6cab003abbc3e8f5b97a094c15c4091c
hyejin-1010 <hyepago@gmail.com>
2023-08-17 18:34:33 +0900

    Add: TheLayout, MainLayout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <LayoutHeader />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import LayoutHeader from '@/layouts/LayoutHeader.vue';
+</script>
+
+<style scoped>
+
+</style>
