0c9a4d020cf1593d8f7a80606567526f8412b790
hyejin-1010 <hyepago@gmail.com>
2023-09-20 10:12:30 +0900

    Add: 모바일인 경우 하단 Tabs

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <q-layout view="lHh LpR lFf" container class="rounded-borders">
-    <q-drawer :modelValue="true" :width="70"
+    <!-- Side Bar -->
+    <q-drawer v-if="!isMobile" :modelValue="true" :width="70"
       :breakpoint="700" side="left" class="flex flex-col shadow-lg">
       <div class="p-2">
         <img src="@/assets/images/logo.png" alt="로고" />
@@ -37,6 +38,33 @@
         </div>
       </q-page>
     </q-page-container>
+
+    <!-- 하단 Tabs -->
+    <q-footer v-if="isMobile" class="grid shadow-[0px_-5px_20px] shadow-slate-300 grid-cols-5">
+      <template v-for="(menu, index) in menus">
+        <router-link :to="menu.to"
+          class="block py-2 text-center"
+          :class="menu.to === currentPath ? 'bg-primary text-white' : 'bg-white text-primary hover:text-primary'">
+          <div>
+            <q-icon :name="menu.icon" size="sm" />
+          </div>
+          <div class="mt-1 text-xs">
+            {{ menu.label }}
+          </div>
+        </router-link>
+
+        <div v-if="index === 1" class="flex flex-col text-center bg-white">
+          <img src="@/assets/images/logo.png" alt="로고"
+            class="w-2/3 m-auto" />
+        </div>
+      </template>
+
+      <div class="flex flex-col justify-center text-center text-gray-600 bg-white"
+        @click="loggedIn ? onClickLogoutBtn() : onClickLoginBtn()">
+        <q-icon name="logout" size="sm" class="mx-auto" />
+        <div class="justify-center mt-1 text-xs text-center">{{ loggedIn ? '로그아웃' : '로그인' }}</div>
+      </div>
+    </q-footer>
   </q-layout>
 
   <SignInDialog :show="isOpenSignInDialog"
@@ -46,8 +74,9 @@
 <script setup lang="ts">
 import { onBeforeMount, ComputedRef, computed, ref, Ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { useQuasar } from 'quasar';
 import { useUserStore } from '@/stores/user';
-import SignInDialog from "@/dialogs/SignInDialog.vue"
+import SignInDialog from '@/dialogs/SignInDialog.vue';
 
 interface Menu {
   icon: string;
@@ -55,20 +84,22 @@ interface Menu {
   to: string;
 }
 
+const $q = useQuasar();
 const route = useRoute();
 const userStore = useUserStore();
 
+const isMobile: ComputedRef<boolean> = computed(() => $q.screen.xs);
 const currentPath: ComputedRef<string> = computed(() => route.path);
 const loggedIn: ComputedRef<boolean> = computed(() => userStore.loggedIn);
-const menus: ComptuedRef<Menu[]> = computed(() => {
+const menus: ComputedRef<Menu[]> = computed(() => {
   let menus = [
     { icon: 'map', label: '맵', to: '/cafeMap' } as Menu,
     { icon: 'favorite', label: '즐겨찾기', to: '/favorite' } as Menu,
     { icon: 'forum', label: '소통창구', to: '/communication-board' } as Menu,
   ];
   // 어드민인 경우에만 어드민 메뉴 추가
-  if (userStore.isAdmin) {
-    menus = [...menus, { icon: 'admin_panel_settings', label: '어드민', to: '/admin' } as Menu];
+  if (!isMobile.value && userStore.isAdmin) {
+    menus = [...menus, { icon: 'admin_panel_settings', label: '어드민', to: '/admin/group' } as Menu];
   }
   return menus;
 });
