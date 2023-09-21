c3cfd21df9080384f49b9f5ee570f2326971ee97
hyejin-1010 <hyepago@gmail.com>
2023-09-21 16:13:27 +0900

    Mod: ArchiveDetail Header 쪽 수정

--- src/pages/ArchiveDetail.vue
+++ src/pages/ArchiveDetail.vue
@@ -1,20 +1,41 @@
 <template>
-  <layout-header></layout-header>
   <div v-if="archive" :style="`background-image: url(${mainImage})`"
-    class="relative text-white bg-center bg-cover h-80 sm:h-64">
+    class="relative text-white bg-center bg-cover h-96 sm:h-80">
     <div class="absolute top-0 left-0 w-full h-full bg-black/70"></div>
-    <div class="absolute top-1/2 translate-y-[-50%] left-40">
-      <div class="text-2xl font-black">
-        <span class="text-primary">{{ archive.artist?.name }}</span> 생일 카페
+    <div class="absolute top-1/2 translate-y-[-50%] left-28">
+      <div class="text-2xl font-black text-primary">
+        {{ archive.artist?.name }} 생일 카페
       </div>
-      <div class="archive-theme text-5xl font-black">
-        {{ archive.themeName }}
+
+      <div class="mt-2 mb-4 font-black archive-theme">
+        <span class="inline-block w-10 h-10 mr-2 text-3xl text-left align-middle rounded-full cursor-pointer"
+          @click="onClickFavoriteIcon">
+          <q-icon :name="`favorite${!archive.favorite && '_outline'}`" class="!inline m-auto text-red-600" />
+        </span>
+        <div class="inline-block text-5xl align-middle">
+          {{ archive.themeName }}
+        </div>
       </div>
-      <div class="text-lg text-gray-400">{{ archive.organizer }}</div>
+
+      <!-- 해시태그 : 지역, 아티스트, 실제 카페 명 -->
+      <div>
+        <!-- 그룹명 -->
+        <div :class="classes.hashtag">
+          #{{ archive.group.name }}
+        </div>
+        <!-- 아티스트 -->
+        <div :class="classes.hashtag">
+          #{{ archive.artist.name }}
+        </div>
+        <!-- 실제 카페 명 -->
+        <div :class="classes.hashtag">
+          #{{ archive.name }}
+        </div>
+      </div>
     </div>
   </div>
 
-  <div class="px-40">
+  <div class="px-28">
     <div class="inline-block pt-20 pb-2 text-3xl font-bold border-b-2 border-black">기본 정보</div>
 
     <div v-if="archive" class="text-xl text-gray-800 mt-11">
@@ -53,6 +74,8 @@
 import { onBeforeMount, ref, Ref, computed, ComputedRef } from 'vue';
 import { useRoute } from 'vue-router';
 import { useArchiveStore } from '@/stores/archive';
+import { useUserStore } from '@/stores/user';
+import { useFavoriteArchiveStore } from '@/stores/favoriteArchive';
 import { Archive } from '@/types/Archive';
 import { Image } from '@/types/Image';
 import ImageSlide from '@/components/common/imageSlide.vue';
@@ -63,8 +86,14 @@ import {useQuasar} from "quasar"
 const $q = useQuasar()
 const route = useRoute();
 const archiveStore = useArchiveStore();
+const favoriteArchiveStore = useFavoriteArchiveStore();
+const userStore = useUserStore();
 
 const archive: Ref<Archive | undefined> = ref();
+
+const classes: Record<string, string> = {
+  hashtag: 'inline-block px-3 py-1 mr-2 border border-white rounded-full'
+};
 
 $q.screen.setSizes({sm: 360, md: 800, lg: 1500, xl: 2000});
 const height: ComputedRef<string | undefined> = computed(() => {
@@ -125,6 +154,22 @@ async function getArchive() {
 
 function numToTime(num: number): string {
   return num < 10 ? `0${num}` : String(num);
+}
+
+async function onClickFavoriteIcon() {
+  // 로그인되어 있지 않은 경우, 경고 메세지를 띄워준다.
+  if (!userStore.loggedIn) { return $q.notify('즐겨찾기 기능은 로그인 시 이용 가능합니다.'); }
+
+  let success: any = false;
+  try {
+    if (archive.value.favorite) {
+      success = await favoriteArchiveStore.doRemoveFavorite(archive.value._id);
+    } else {
+      success = await favoriteArchiveStore.doCreateFavorite(archive.value._id);
+    }
+    if (!success) { return; }
+    archive.value.favorite = !archive.value.favorite;
+  } catch (_) {}
 }
 
 </script>
