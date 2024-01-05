ce8209058737fa8add63ac4fde91e1a1e20db5ae
hyejin-1010 <hyepago@gmail.com>
2024-01-05 23:34:22 +0900

    feat. FavoriteArchive - 전체 리스트 보기 버튼 + 현재 즐겨찾기 그룹의 정보 표시

--- src/pages/FavoriteArchives.vue
+++ src/pages/FavoriteArchives.vue
@@ -1,8 +1,33 @@
 <template>
   <div>
-    <q-list>
-      <CafeItem v-for="archive in archives" :archive="archive" @click="onClickArchive(archive)" />
-    </q-list>
+    <div class="mx-4 text-center" @click="goToList">
+      <button
+        class="w-full py-2 font-bold text-gray-600 border border-gray-600 rounded-lg hover:border-primary hover:text-primary"
+      >
+        전체 리스트 보기
+      </button>
+    </div>
+
+    <template v-if="favoriteGroup">
+      <!-- 현재 Favorite Group에 대한 정보 -->
+      <div class="mt-6 mb-4 text-center">
+        <div class="inline-block align-middle">
+          <div
+            class="flex flex-col justify-center w-5 h-5 mr-2 text-center rounded-full"
+            :style="`background-color: ${favoriteGroup.color}`"
+          >
+            <q-icon name="favorite" class="m-auto text-white" />
+          </div>
+        </div>
+
+        <div class="inline-block text-xl font-bold align-middle">{{ favoriteGroup.title }}</div>
+      </div>
+
+      <!-- 즐겨찾기 그룹 내 카페 리스트 -->
+      <q-list>
+        <CafeItem v-for="archive in favoriteGroup.archives" :archive="archive" @click="onClickArchive(archive)" />
+      </q-list>
+    </template>
   </div>
 </template>
 
@@ -24,7 +49,7 @@ const router = useRouter();
 
 const mapStore = useMapStore();
 
-const archives: Ref<Archive> = ref([]);
+const favoriteGroup = ref(); // 즐겨찾기 그룹 정보
 
 onBeforeMount(() => {
   const id = route.params.id;
@@ -33,8 +58,8 @@ onBeforeMount(() => {
 
 function getData(id: string) {
   query(getFavoritesGroup, { id }, false).then(({ data, error }) => {
-    archives.value = data.value.favoriteGroup?.archives || [];
-    mapStore.setMarkerData(archives.value);
+    favoriteGroup.value = data.value.favoriteGroup;
+    mapStore.setMarkerData(favoriteGroup.value?.archives || []);
   });
 }
 
@@ -45,6 +70,11 @@ function onClickArchive(archive: Archive) {
   } else {
     mapStore.setSelectedArchive(archive);
   }
+}
+
+// 전체 리스트 보기
+function goToList() {
+  router.push('/favorite');
 }
 </script>
 
