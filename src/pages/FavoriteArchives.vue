9a779ef54e10c1ba1c9df81ea086f2728b65ab9c
hyejin-1010 <hyepago@gmail.com>
2024-01-05 23:15:31 +0900

    feat. FavoriteArchive - CateItem component 사용하게끔

--- src/pages/FavoriteArchives.vue
+++ src/pages/FavoriteArchives.vue
@@ -1,32 +1,50 @@
 <template>
   <div>
-    <div v-for="archive in archives">
-      {{ archive.name }}
-    </div>
+    <q-list>
+      <CafeItem v-for="archive in archives" :archive="archive" @click="onClickArchive(archive)" />
+    </q-list>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, Ref, onBeforeMount } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
+import { useQuasar } from 'quasar';
 
 import { query } from '@/composables/graphqlUtils';
-import getFavoritesGroup from '@/graphql/getFavoritesGroupItem.query.gql';
 import { Archive } from '@/types/Archive';
+import { useMapStore } from '@/stores/map';
+import CafeItem from '@/components/CafeItem.vue';
 
+import getFavoritesGroup from '@/graphql/getFavoritesGroupItem.query.gql';
+
+const $q = useQuasar();
 const route = useRoute();
+const router = useRouter();
+
+const mapStore = useMapStore();
 
 const archives: Ref<Archive> = ref([]);
 
 onBeforeMount(() => {
   const id = route.params.id;
-  getItem(id);
+  getData(id);
 });
 
-function getItem(id: string) {
+function getData(id: string) {
   query(getFavoritesGroup, { id }, false).then(({ data, error }) => {
     archives.value = data.value.favoriteGroup?.archives || [];
+    mapStore.setMarkerData(archives.value);
   });
+}
+
+// 카페 클릭 시
+function onClickArchive(archive: Archive) {
+  if ($q.screen.xs) {
+    router.push(`/archive/${archive._id}`);
+  } else {
+    mapStore.setSelectedArchive(archive);
+  }
 }
 </script>
 
